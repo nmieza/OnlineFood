0615c6a559509e81cbe60d03a699d395f914f753
nmieza <nurmieza14@gmail.com>
2022-06-22 12:16:20 +0800

    commit

--- confirm-order.php
+++ confirm-order.php
@@ -199,7 +199,7 @@ if($continue){
         <i class="mdi-content-content-paste red circle"></i>
         <p><strong>Name:</strong>'.$name.'</p>
 		<p><strong>Contact Number:</strong> '.$contact.'</p>
-		<p><strong>Address:</strong> '.htmlspecialchars($_POST['address']).'</p>	
+		<p><strong>Table No:</strong> '.htmlspecialchars($_POST['address']).'</p>	
 		<p><strong>Payment Type:</strong> '.$_POST['payment_type'].'</p>			
         <a href="#" class="secondary-content"><i class="mdi-action-grade"></i></a>';
 		
