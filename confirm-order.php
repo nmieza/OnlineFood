903fd332680ea3c46702ec165c84cb8e2118581a
nmieza <nurmieza14@gmail.com>
2022-06-29 11:38:17 +0800

    commit

--- confirm-order.php
+++ confirm-order.php
@@ -82,9 +82,7 @@ if($continue){
                     <ul class="left">                      
                       <li><h1 class="logo-wrapper"><a href="index.php" class="brand-logo darken-1"><img src="images/materialize-logo.png" alt="logo"></a> <span class="logo-text">Logo</span></h1></li>
                     </ul>
-                    <ul class="right hide-on-med-and-down">                        
-                        <li><a href="#" class="waves-effect waves-block waves-light"><i class="mdi-editor-attach-money"><?php echo $balance;?></i></a>
-                        </li>
+                    <ul class="right hide-on-med-and-down">                       
                     </ul>					
                 </div>
             </nav>
