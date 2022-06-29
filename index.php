4ed3a59814bd0f82078089184ef0ad94ec2bf42c
nmieza <nurmieza14@gmail.com>
2022-06-29 10:56:32 +0800

    commit

--- index.php
+++ index.php
@@ -153,13 +153,7 @@ include 'includes/wallet.php';
                         </li>
                     </ul>
                 </li>
-                <li class="no-padding">
-                    <ul class="collapsible collapsible-accordion">
-                        <li class="bold"><a class="collapsible-header waves-effect waves-cyan"><i class="mdi-action-question-answer"></i> Tickets</a>
-                            <div class="collapsible-body">
-                                <ul>
-								<li><a href="tickets.php">All Tickets</a>
-                                </li>
+               
 								<?php
 									$sql = mysqli_query($con, "SELECT DISTINCT status FROM tickets WHERE poster_id = $user_id AND not deleted;");
 									while($row = mysqli_fetch_array($sql)){
