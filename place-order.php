19746ddaa8c55ab4ec5c909c3b6a80c24b7340be
nmieza <nurmieza14@gmail.com>
2022-06-29 11:37:03 +0800

    commit

--- place-order.php
+++ place-order.php
@@ -105,8 +105,6 @@ $verified = $row['verified'];
                       <li><h1 class="logo-wrapper"><a href="index.php" class="brand-logo darken-1"><img src="images/materialize-logo.png" alt="logo"></a> <span class="logo-text">Logo</span></h1></li>
                     </ul>
                     <ul class="right hide-on-med-and-down">                        
-                        <li><a href="#" class="waves-effect waves-block waves-light"><i class="mdi-editor-attach-money"><?php echo $balance;?></i></a>
-                        </li>
                     </ul>						
                 </div>
             </nav>
